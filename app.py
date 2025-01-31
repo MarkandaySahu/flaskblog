b8014de97139861d5f22cd7bd2e49d1a8b8cc15c
markanday sahu <78273939+MarkandaySahu@users.noreply.github.com>
2025-01-31 12:43:44 +0530

    error handling

--- app.py
+++ app.py
@@ -5,3 +5,18 @@ app = Flask("__name__")
 @app.route("/")
 def index():
     return "hello world"
+
+#invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+#internal server error
+@app.errorhandler(500)
+def server_error(e):
+    return render_template("500.html"), 500
+
+#for any other exception
+@app.errorhandler(Exception)
+def handle_exception(e):
+    return render_template("error.html",error=e), 500
